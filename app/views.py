3080261b87ec562b505b0fced8dc468dc5cdf700
harshadval11 <harshadvali1431@gmail.com>
2023-08-25 11:52:47 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,78 @@
 from django.shortcuts import render
 
 # Create your views here.
+from app.models import *
+
+def display_topics(request):
+    QSTO=Topic.objects.all()
+    d={'QSTO':QSTO}
+    return render(request,'display_topics.html',d)
+
+
+def display_webpages(request):
+    QSWO=Webpage.objects.all()
+    d={'QSWO':QSWO}
+    return render(request,'display_webpages.html',d)
+
+
+def display_access(request):
+    QSAO=AccessRecord.objects.all()
+    d={'QSAO':QSAO}
+    return render(request,'display_access.html',d)
+
+
+def insert_topic(request):
+    topic_name=input('enter topic_name')
+    TO=Topic.objects.get_or_create(topic_name=topic_name)[0]
+    TO.save()
+    QSTO=Topic.objects.all()
+    d={'QSTO':QSTO}
+    return render(request,'display_topics.html',d)
+
+
+
+def insert_webpage(request):
+    tn=input('enter topic_name')
+    na=input('enter name')
+    ur=input('enter url')
+
+    TO=Topic.objects.get(topic_name=tn)
+
+    WO=Webpage.objects.get_or_create(topic_name=TO,name=na,url=ur)[0]
+    WO.save()
+
+    QSWO=Webpage.objects.all()
+    d={'QSWO':QSWO}
+    return render(request,'display_webpages.html',d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- project18/urls.py
+++ project18/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('display_topics/',display_topics,name='display_topics'),
+    path('display_webpages/',display_webpages,name='display_webpages'),
+    path('display_access/',display_access,name='display_access'),
+    path('insert_topic/',insert_topic,name='insert_topic'),
+    path('insert_webpage/',insert_webpage,name='insert_webpage'),
 ]
 
 
